9d3747d3e3eec727281f7e1a4ec0829f28ba87a4
takahiro-tec <malkdaisuki777@gmail.com>
2024-07-24 20:26:59 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,7 @@
 // JavaScriptをもちいて、以下の体重と身長のBMI（体格指数）を出力できるプログラムを記述する課題です
 
-let 体重 = 68.0; // kg
-let 身長 = 1.7; // m
+let taiJou = 68.0; // kg
+let shinCho = 1.7; // m
 
-let BMI = 体重 / (身長 * 身長);
+let BMI = taiJou / (shinCho * shinCho);
 console.log("BMI（体格指数）は " + BMI + " です。");
